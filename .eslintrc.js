8c245c5b4f8ebba1652f76eda9fccbe946c7d799
nmwh <43128952+do0401@users.noreply.github.com>
2019-11-21 17:25:03 +0900

    text editor commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
     'plugin:vue/recommended',
     '@vue/standard',
     'eslint:recommended'
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.athlete.ve', // 앱 식별자로써 매우 중요한 역할을 함
+        nsis: {
+          shortcutName: 'veApp'
+        },
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: [
+                'x64',
+                'ia32'
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
 }
